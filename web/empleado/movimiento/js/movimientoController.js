5389972f1d5cf4cd813dd013ee21f2f3ad07baff
Manu <Manu@PC>
2014-10-23 19:02:08 +0200

    controllador detail movimiento 2

--- web/empleado/movimiento/js/movimientoController.js
+++ web/empleado/movimiento/js/movimientoController.js
@@ -1,20 +1,32 @@
 
- var app =  angular.module("app",[]);
- 
- app.controller("movimientoDetailController", function ($scope,$http){
-     
-     
-     $scope.getMovimiento= function(){
-       $http({
-           method:"GET",
-           url: contextPath + "/api/movimiento/" + $scope.movimiento.id
-       }).success(function(data,status){
-           $scope.movimiento=data;
-       }).error(function (data,status){
-           alert("Error"+status);
-       });
-         
-         
-     };
-     
- });
+var app = angular.module("app", []);
+
+app.controller("movimientoDetailController", function ($scope, $http) {
+
+
+    $scope.getMovimiento = function () {
+        $http({
+            method: "GET",
+            url: contextPath + "/api/movimiento/" + $scope.movimiento.id
+        }).success(function (data, status) {
+            $scope.movimiento = data;
+        }).error(function (data, status) {
+            alert("Error" + status);
+        });
+
+
+    };
+
+    $scope.insertMovimiento = function () {
+        $http({
+            method: "POST",
+            data: $scope.movimiento,
+            url: contextPath + "/api/movimiento/"
+        }).success(function (data, status) {
+            alert("funciona" + data);
+        }).error(function (data, status) {
+            alert("NO funciona"+ status);
+        });
+    };
+
+});
