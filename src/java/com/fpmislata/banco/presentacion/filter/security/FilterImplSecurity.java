f558f3fc87c24b840af385c32c0bca6bf4ee2e3d
alumno <alumno@172.16.205.25>
2014-11-28 10:03:39 +0100

    Reparado filtro (estaba la versión vieja)

--- src/java/com/fpmislata/banco/presentacion/filter/security/FilterImplSecurity.java
+++ src/java/com/fpmislata/banco/presentacion/filter/security/FilterImplSecurity.java
@@ -1,18 +1,20 @@
 package com.fpmislata.banco.presentacion.filter.security;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class FilterImplSecurity implements Filter {
 
     FilterConfig filterConfig = null;
+    private HttpSession httpsession;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -28,9 +30,12 @@ public class FilterImplSecurity implements Filter {
             throws IOException, ServletException {
         servletResponse.setContentType("text/html");
 
-        PrintWriter out = servletResponse.getWriter();
-        out.println("");
-        if (false) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String sessionUrl = httpServletRequest.getContextPath() + "/api/session";
+        System.out.println(httpServletRequest.getRequestURI());
+        System.out.println(sessionUrl);
+        httpsession = httpServletRequest.getSession();
+        if (httpsession.getAttribute("name") != null || httpServletRequest.getRequestURI().equals(sessionUrl)) {
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
             /* Return error */
