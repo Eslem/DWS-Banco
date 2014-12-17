525d5bb376322a36fd27225b78ede36c8cc685f3
alumno <manuguionbajo@gmail.com>
2014-12-17 13:45:12 +0100

    banco bootstrap

--- web/js/index.js
+++ web/js/index.js
@@ -1,12 +1,40 @@
 
 
 
-$(document).ready(function(){
-    
-    $(".nav ul").find("li").on("click","a",function(){
-       $(".nav ul").find("li").children("a").removeClass("active");
-        $(this).toggleClass("active");
-        
+$(document).ready(function () {
+
+    $(".nav ul a").click(function () {
+        $("header .nav ul a.active").removeClass("active");
+        $(this).addClass("active");
     });
     
+ $(".carousel").carousel({
+     interval:5000
+ });
+
+});
+
+$(function () {
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop != 0)
+            $('.nav').stop().animate({'opacity': '0.2'}, 400);
+        else
+            $('.nav').stop().animate({'opacity': '1'}, 400);
+    });
+
+    $('.nav').hover(
+            function (e) {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop != 0) {
+                    $('.nav').stop().animate({'opacity': '1'}, 400);
+                }
+            },
+            function (e) {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop != 0) {
+                    $('.nav').stop().animate({'opacity': '0.2'}, 400);
+                }
+            }
+    );
 });
